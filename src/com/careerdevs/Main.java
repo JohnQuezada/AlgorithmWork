482721fed345eec735e54961dabc6afe9c59d453
JohnQuezada <johnaquezada@gmail.com>
2021-12-20 21:12:28 -0500

    implement addition algorithm

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -7,9 +7,14 @@ public class Main {
 
         System.out.println(getFirstValue(new int[]{1, 2, 3, 4}));
 
+        System.out.println(addition(2));
     }
 
     public static int getFirstValue(int[] arr) {
         return arr[0];
+    }
+
+    public static int addition(int num) {
+        return num + 1;
     }
 }
