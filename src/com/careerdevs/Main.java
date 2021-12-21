5f9b5d9541550acf9a255588eef7a8ccf8c81983
JohnQuezada <johnaquezada@gmail.com>
2021-12-20 22:52:20 -0500

    implemented additional challenge algorithm

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -8,6 +8,12 @@ public class Main {
         System.out.println(getFirstValue(new int[]{1, 2, 3, 4}));
 
         System.out.println(addition(2));
+
+        System.out.println(search(new int[]{1, 2, 3, 4},3));
+
+        System.out.println(convert(6));
+
+        System.out.println(giveMeSomething("a"));
     }
 
     public static int getFirstValue(int[] arr) {
@@ -16,5 +22,22 @@ public class Main {
 
     public static int addition(int num) {
         return num + 1;
+    }
+
+    public static int search(int[] arr, int item) {
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(arr[i]==item)
+                return i;
+        }
+        return -1;
+    }
+
+    public static int convert(int minutes) {
+        return minutes * 60;
+    }
+
+    public static String giveMeSomething(String a) {
+        return "something " + a;
     }
 }
