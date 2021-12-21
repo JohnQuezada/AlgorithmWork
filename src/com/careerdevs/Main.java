c64dc1df6deb38829dfd26f9b736c050eb4a77a5
JohnQuezada <johnaquezada@gmail.com>
2021-12-20 20:56:59 -0500

    Initial commit

---
+++ src/com/careerdevs/Main.java
@@ -0,0 +1,15 @@
+package com.careerdevs;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(getFirstValue(new int[]{1, 2, 3, 4}));
+
+    }
+
+    public static int getFirstValue(int[] arr) {
+        return arr[0];
+    }
+}
