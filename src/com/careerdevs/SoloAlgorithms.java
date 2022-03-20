fd3b4f0f3d73f0d8b2bc1704ae8ae71544c3d391
JohnQuezada <johnaquezada@gmail.com>
2022-03-20 17:40:11 -0400

    new solo algorithm

--- src/com/careerdevs/SoloAlgorithms.java
+++ src/com/careerdevs/SoloAlgorithms.java
@@ -1,7 +1,15 @@
 package com.careerdevs;
 
+import java.util.Arrays;
+
 public class SoloAlgorithms {
+
     public static void main(String[] args) {
-        
+
+        System.out.println(Arrays.toString(numberSplit(4)));
+    }
+
+    public static int[] numberSplit(int n) {
+        return new int[] {(int)Math.floor(n/2.0), (int)Math.ceil(n/2.0)};
     }
 }
