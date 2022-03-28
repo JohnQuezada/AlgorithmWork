73b9b7cc1a9bf870c70d9626c6ae06c2d1a74d96
JohnQuezada <johnaquezada@gmail.com>
2022-03-28 18:40:48 -0400

    Hacker Rank Day 7

---
+++ src/com/careerdevs/HackerRank/Day07Arrays.java
@@ -0,0 +1,26 @@
+package com.careerdevs.HackerRank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Day07Arrays {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        bufferedReader.close();
+
+        // A = [1, 2, 3, 4]
+        for (int i = arr.size() - 1; i >= 0; i--)
+            System.out.print(arr.get(i) + " ");
+    }
+}
