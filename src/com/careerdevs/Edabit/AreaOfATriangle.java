93f9bbcc3594ffcd7ade01f714644876daf5f81e
JohnQuezada <johnaquezada@gmail.com>
2022-04-12 18:20:47 -0400

    4/12 warmup

---
+++ src/com/careerdevs/Edabit/AreaOfATriangle.java
@@ -0,0 +1,13 @@
+package com.careerdevs.Edabit;
+
+public class AreaOfATriangle {
+
+    public static void main(String[] args) {
+        triArea(3, 2);
+    }
+
+    public static int triArea(int base, int height) {
+        int solution = (base * height) / 2;
+        return solution;
+    }
+}
