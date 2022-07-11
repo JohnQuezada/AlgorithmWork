b239e965319ea452089c8517307c60c6b20e12dd
JohnQuezada <johnaquezada@gmail.com>
2022-07-11 18:36:13 -0400

    new algorithm addition

---
+++ src/com/careerdevs/Edabit/IsTheNumberLessThanOrEqualToZero.java
@@ -0,0 +1,10 @@
+package com.careerdevs.Edabit;
+
+public class IsTheNumberLessThanOrEqualToZero {
+    public static boolean lessThanOrEqualToZero(int num) {
+        if (num <= 0) {
+            return true;
+        }
+        return false;
+    }
+}
