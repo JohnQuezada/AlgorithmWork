c20189c3fe839d3fc2bc1fa635a2366ecc18d16b
JohnQuezada <johnaquezada@gmail.com>
2022-07-13 22:04:33 -0400

    new algorithm addition

---
+++ src/com/careerdevs/Edabit/CaptureTheRook.java
@@ -0,0 +1,10 @@
+package com.careerdevs.Edabit;
+
+public class CaptureTheRook {
+    public static boolean canCapture(String[] rooks) {
+        if (rooks[0].charAt(0) == rooks[1].charAt(0) || rooks[0].charAt(1) == rooks[1].charAt(1)) {
+            return true;
+        }
+        return false;
+    }
+}
